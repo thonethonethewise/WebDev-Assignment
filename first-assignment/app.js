ab9a539f861da61dc95339c00dddfbc46baa7623
thonethonethewise <suwathonenwey@gmail.com>
2022-09-17 16:32:44 +0630

    first assignment

---
+++ first-assignment/app.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+
+    fs.readFile('./index.html', function(error, data){
+        if(error){
+            res.writeHead(404, {'Content-Type' : 'text/html'})
+            return res.end('404 not found');
+        } 
+        res.writeHead(202, {'Content-Type' : 'text/html'})
+        res.write(data)
+        return res.end()
+    })
+
+}).listen(3000);
